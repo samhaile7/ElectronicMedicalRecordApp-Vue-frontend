8450d625e09b945535f8e791e83f76dd25e79982
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-11 23:18:25 -0400

    Added data to MasterPatientList component

--- src/components/MasterPatientList.vue
+++ src/components/MasterPatientList.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class= "masterlist">
-    <h1>list goes here</h1>
+  <div class="masterlist">
+    <h1>{{ placeholder }}</h1>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      placeholder: 'master list goes here'
 
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
