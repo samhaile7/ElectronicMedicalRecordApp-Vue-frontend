d0b9cfdabfeac1d82427b4c62e67473682a07e4d
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-11 23:17:45 -0400

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import MasterPatientList from './components/MasterPatientList.vue';
+
+
 </script>
 <!-- defines the template for your page-->
 <template>
