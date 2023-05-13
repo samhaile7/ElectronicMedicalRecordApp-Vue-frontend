6922be2599728a81d543cb95336b5e6075b60984
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-12 23:32:49 -0400

    Uninstalled Pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -12,7 +11,7 @@ axios.defaults.baseURL = 'http://localhost:8080';
 
 const app = createApp(App)
 
-app.use(createPinia())
+
 app.use(router)
 
 app.mount('#app')
