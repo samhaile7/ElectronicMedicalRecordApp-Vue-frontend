18f6c4e24d87532906d8a8f2d5598809487c9348
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-16 22:49:43 -0400

    Removed firebase

--- src/main.js
+++ src/main.js
@@ -6,25 +6,6 @@ import axios from 'axios'
 
 import './assets/main.css'
 
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCRWcPMQS2Y6O1-thbt72oEYTvLQPbIoq0",
-  authDomain: "emrauth-95284.firebaseapp.com",
-  projectId: "emrauth-95284",
-  storageBucket: "emrauth-95284.appspot.com",
-  messagingSenderId: "408541018684",
-  appId: "1:408541018684:web:c0ce8732a3e78b3b21a139"
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
-
 // eslint-disable-next-line no-undef
 axios.defaults.baseURL = 'http://localhost:8080';
 
