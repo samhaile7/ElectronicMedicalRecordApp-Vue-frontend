8e46bf5271fde0dfc82c218fd898abaa3f39a9b3
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-16 22:10:00 -0400

    Added 404 component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,6 +23,7 @@ const router = createRouter({
       name: 'feed',
       component: () => import('../views/Feed.vue')
     },
+    { path: '*', component: () => import('../views/Feed.vue') }
   ]
 })
 
---
+++ src/views/NotFoundComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  404 Not found
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
