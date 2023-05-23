02e4178e9214adf71b3ea47e124ed1deb363b1e9
Samuel Haile <110855004+samhaile7@users.noreply.github.com>
2023-05-23 14:46:36 -0400

    MAster table css

--- src/components/MasterPatientList.vue
+++ src/components/MasterPatientList.vue
@@ -1,42 +1,60 @@
 <template>
   <div class="masterlist">
-
     <button v-on:click="listAllPatients()">Get master list</button>
+    
 
-    <h1>{{ allPatients }}</h1>
-    <h1>Components to include </h1>
+    <el-table v-if="showTable" :data="allPatients" height="250">
+      <el-table-column prop="patientId" label="patientId" />
+      <el-table-column prop="firstName" label="First" />
+      <el-table-column prop="lastName" label="Last" />
+
+      <el-table-column prop="birthDate" label="Birthday" />
+    
+        <el-table-column prop="pulseRate" label="Pulse (BPM)" />
+        <el-table-column prop="respirationRate" label="Respiratory Rate (RR)" />
+        <el-table-column prop="temperature" label="Temp(F)" />
+        <el-table-column prop="partialThromboplastinTime" label="PTT" />
+        <el-table-column prop="systolicBloodPressure" label="Systolic Pressure" />
+        <el-table-column prop="diastolicBloodPressure" label="Diastolic Pressure" />
+        <el-table-column prop="mobilityStatusId" label="mobilityStatusId" />
+    
+    </el-table>
   </div>
 </template>
 
 <script>
-import PatientService from '../services/PatientService';
+import PatientService from '../services/PatientService'
 
 export default {
   data() {
     return {
-      allPatients: 'No patients here'
-
+      allPatients: [],
+      showTable: false
     }
   },
 
   methods: {
     listAllPatients() {
-      PatientService.listAllPatients().then((response) => {
-        if (response.status === 200) {
-          this.allPatients = response.data;
-        }
-      }).catch((err) => console.log(err));
+      PatientService.listAllPatients()
+        .then((response) => {
+          if (response.status === 200) {
+            this.allPatients = response.data
+            this.showTable = true
+          }
+        })
+        .catch((err) => console.log(err))
     }
   }
 }
 </script>
 
 <style>
-
 .masterlist {
-
-  border: 0.5rem;
-  border-style: dashed;
-  border-color: crimson;
+  color: darkkhaki;
+  background-color: lightyellow;
+  border: 1px solid darkgoldenrod;
+  border-radius: 8px;
+  padding: 10px;
+  margin: 10px;
 }
 </style>
